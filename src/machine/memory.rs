9437478033e4fe759ca68ea76f77bb82d7e9f161
Sindre Stephansen <sindre@sindrestephansen.com>
2019-05-09 21:40:54 +0200

    Add basic tests

--- src/machine/memory.rs
+++ src/machine/memory.rs
@@ -15,3 +15,37 @@ impl MemoryModule {
         self.memory.splice(start..end, data.iter().cloned());
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn insert() {
+        let data = vec![1, 2, 3, 4, 5, 6, 7];
+        let size: usize = 10;
+        let mut memory = MemoryModule::new(size);
+
+        memory.insert(&data, 0);
+
+        for i in 0..size {
+            if i < data.len() {
+                assert_eq!(data[i], memory.memory[i]);
+            } else {
+                assert_eq!(0, memory.memory[i]);
+            }
+        }
+
+        memory.insert(&data, size - data.len());
+
+        for i in 0..size {
+            if i >= size - data.len() {
+                assert_eq!(data[i - (size - data.len())], memory.memory[i]);
+            } else if i < data.len() {
+                assert_eq!(data[i], memory.memory[i]);
+            } else {
+                assert_eq!(0, memory.memory[i]);
+            }
+        }
+    }
+}
--- src/machine/registers.rs
+++ src/machine/registers.rs
@@ -15,3 +15,23 @@ impl Register {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn name() {
+        let reg1 = Register::new(1, None);
+        assert_eq!("r1", reg1.name);
+        assert_eq!("r1", reg1.base_name);
+
+        let reg2 = Register::new(2, None);
+        assert_eq!("r2", reg2.name);
+        assert_eq!("r2", reg2.base_name);
+
+        let reg3 = Register::new(3, Some("third"));
+        assert_eq!("third", reg3.name);
+        assert_eq!("r3", reg3.base_name);
+    }
+}
